1de10f0a9bcf9b7893c9ea7c784e2b29b2e851a4
yeasinshakil <shakilrock1998@gmail.com>
2023-07-12 16:14:58 +0600

    project setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover & Share AI Prompts'
+}
+
+function Rootlayout({children}) {
+  return (
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default Rootlayout
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+
+function Home() {
+  return (
+    <section className=" w-full flex-center flex-col">
+        <h1 className=" text-center head_text">
+            Discover & Share
+            <br className=" max-md:hidden"/>
+            <span className="orange_gradient text-center">AI Powered Prompts</span>
+        </h1>
+
+        <p className="desc text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo in corporis eligendi sint excepturi ducimus voluptates rem. Optio, dignissimos perspiciatis.</p>
+
+    </section>
+  )
+}
+
+export default Home
